509ca73cf7e12f63e1e1b711b196aed2dd7241ed
Matesma <matesma98@gmail.com>
2018-01-08 09:52:29 +0100

    extra

---
+++ script.js
@@ -0,0 +1,15 @@
+$(window).on("load", start);
+
+function start() {
+    console.log("start");
+
+    $(".tema").fadeIn(1500);
+    $("#intro").fadeIn(1500);
+    $("#content").fadeIn(1500);
+    $("#content1").fadeIn(1500);
+    $("#content2").fadeIn(1500);
+    $("#content3").fadeIn(1500);
+    $("#content4").fadeIn(1500);
+    $("#ending").fadeIn(1500);
+    $("#ending1").fadeIn(1500);
+}
